a4c8053aee72f5e46bc6ab09eb9aa61fba5f777a
vaarigupta <vaarigupta24@gmail.com>
2020-12-22 13:22:49 +0530

    Project modified

---
+++ Docker Kubernetes Microservices/CatalogAPIs/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CatalogAPIs.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Harry Potter", "Revolution 2020", "James Bond", "Science Fiction" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+    }
+}
